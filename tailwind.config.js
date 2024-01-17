e817d859ee7a0e70e01f0262c4ede9d044dc0b66
mason.hows11 <mason.hows11@gmail.com>
2024-01-17 18:43:31 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+
+      },
+      spacing:{
+        
+      },
+      fontFamily:{
+          iransans : ['iransans','IRANSansWeb'],
+          vazirmediumfd : ['VazirMediumFD'],
+          vazirmedium : ['VazirMedium'],
+          vazirregular : ['VazirRegular'],
+
+      }
+    },
+  },
+  plugins: [],
+}
+
